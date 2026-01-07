a274922cc8cbb015c05e6b459da4b2723bb077c4
ACT-amol <amol@yourdomain.com>
2026-01-07 07:09:16 +0000

    Saving work before pull

--- run_backup.sh
+++ run_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ HEAD
 
 # Move to the permanent volume directory
 cd /opt/netbox/netbox/scripts/automation
@@ -16,3 +17,11 @@ if command -v git &> /dev/null; then
 else
     echo "Notice: Git not found inside container. Skipping push. Files are saved locally."
 fi
+# Move to the project directory inside the container
+cd /opt/netbox/netbox/scripts/automation
+
+# Use the virtual environment's python to run the build
+./venv/bin/python3 scripts/build_configs.py
+
+# Optional: Add your git commands here later
+>>>>>>> b75fcf8 (Saving work before pull)
